215ed1fa7da9208c31fe5dc32f02f52ea57f4279
cmarshak <charles.marshak@gmail.com>
2021-02-16 17:06:02 -0800

    overwrite sym links

--- tests/coseismic_standard_product_pipeline_2_full/symbolic_link_data_2.sh
+++ tests/coseismic_standard_product_pipeline_2_full/symbolic_link_data_2.sh
@@ -1,2 +1,2 @@
-ln -s ../coseismic_standard_product_pipeline_2/S1*_IW_SLC__**.zip .
-ln -s ../coseismic_standard_product_pipeline_2/S1*_OPER_AUX_POEORB_OPOD_*.EOF .
+ln -ns ../coseismic_standard_product_pipeline_2/S1*_IW_SLC__**.zip .
+ln -ns ../coseismic_standard_product_pipeline_2/S1*_OPER_AUX_POEORB_OPOD_*.EOF .
