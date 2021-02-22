a02809e483453b0db1b8823853cd927464c5d212
cmarshak <charles.marshak@gmail.com>
2021-02-22 10:57:05 -0800

    fix download script

--- tests/download.sh
+++ tests/download.sh
@@ -2,7 +2,7 @@ cd coseismic_standard_product_pipeline_1
 python download_asf_slcs_1.py
 bash download_eofs_1.sh
 
-cd ../standard_product_pipeline_2
+cd ../coseismic_standard_product_pipeline_2
 python download_asf_slcs_2.py
 bash download_eofs_2.sh
 
@@ -12,10 +12,10 @@ bash symbolic_link_data_1.sh
 cd ../standard_product_pipeline_2
 bash symbolic_link_data_2.sh
 
-cd ../standard_product_pipeline_1_full
+cd ../coseismic_standard_product_pipeline_1_full
 bash symbolic_link_data_1.sh
 
-cd ../standard_product_pipeline_2_full
+cd ../coseismic_standard_product_pipeline_2_full
 bash symbolic_link_data_2.sh
 
 cd ..
