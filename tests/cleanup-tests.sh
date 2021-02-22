cadc8ba2bd7fb70472499e4c29468b7944dc40f0
cmarshak <charles.marshak@gmail.com>
2021-02-22 09:45:52 -0800

    add cleanup test script

---
+++ tests/cleanup-tests.sh
@@ -0,0 +1,18 @@
+setopt extended_glob
+
+cd standard_product_pipeline_1
+rm -rf -v ^(_context.json|S1*_IW_SLC_*.zip|docker-compose.yaml|*.sh|*.py)
+cd ../standard_product_pipeline_2
+rm -rf -v ^(_context.json|S1*_IW_SLC_*.zip|docker-compose.yaml|*.sh|*.py)
+
+cd ../coseismic_standard_product_pipeline_1
+rm -rf -v ^(_context.json|S1*_IW_SLC_*.zip|docker-compose.yaml|*.sh|*.py)
+cd ../coseismic_standard_product_pipeline_2
+rm -rf -v ^(_context.json|S1*_IW_SLC_*.zip|docker-compose.yaml|*.sh|*.py)
+
+# cd ../coseismic_standard_product_pipeline_1_full
+# rm -rf -v ^(_context.json|S1*_IW_SLC_*.zip|docker-compose.yaml|*.sh|*.py)
+# cd ../coseismic_standard_product_pipeline_2_full
+# rm -rf -v ^(_context.json|S1*_IW_SLC_*.zip|docker-compose.yaml|*.sh|*.py)
+
+cd ..
