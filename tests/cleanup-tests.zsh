b3ec907c49b70605c32e8856eaf579946be4381f
cmarshak <charles.marshak@gmail.com>
2021-02-22 10:40:38 -0800

    make bash and zsh clean up for tests

--- tests/cleanup-tests.zsh
+++ tests/cleanup-tests.zsh
@@ -1,14 +1,14 @@
-shopt -s extglob
+setopt extended_glob
 
 cd standard_product_pipeline_1
-rm -rf -v -- !(_context.json|S1*_IW_SLC_*.zip|docker-compose.yaml|*.sh|*.py|*.EOF)
+rm -rf -v ^(_context.json|S1*_IW_SLC_*.zip|docker-compose.yaml|*.sh|*.py|*.EOF)
 cd ../standard_product_pipeline_2
-rm -rf -v -- !(_context.json|S1*_IW_SLC_*.zip|docker-compose.yaml|*.sh|*.py|*.EOF)
+rm -rf -v ^(_context.json|S1*_IW_SLC_*.zip|docker-compose.yaml|*.sh|*.py|*.EOF)
 
 cd ../coseismic_standard_product_pipeline_1
-rm -rf -v -- !(_context.json|S1*_IW_SLC_*.zip|docker-compose.yaml|*.sh|*.py|*.EOF)
+rm -rf -v ^(_context.json|S1*_IW_SLC_*.zip|docker-compose.yaml|*.sh|*.py|*.EOF)
 cd ../coseismic_standard_product_pipeline_2
-rm -rf -v -- !(_context.json|S1*_IW_SLC_*.zip|docker-compose.yaml|*.sh|*.py|*.EOF)
+rm -rf -v ^(_context.json|S1*_IW_SLC_*.zip|docker-compose.yaml|*.sh|*.py|*.EOF)
 
 # cd ../coseismic_standard_product_pipeline_1_full
 # rm -rf -v ^(_context.json|S1*_IW_SLC_*.zip|docker-compose.yaml|*.sh|*.py|*.EOF)
