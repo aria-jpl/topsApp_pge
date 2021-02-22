4f99eb915c89ba02e86dd23cd066ed20b85ac026
cmarshak <charles.marshak@gmail.com>
2021-02-22 09:58:23 -0800

    ignore EOF in full area (comments)

--- tests/cleanup-tests.sh
+++ tests/cleanup-tests.sh
@@ -11,8 +11,8 @@ cd ../coseismic_standard_product_pipeline_2
 rm -rf -v ^(_context.json|S1*_IW_SLC_*.zip|docker-compose.yaml|*.sh|*.py|*.EOF)
 
 # cd ../coseismic_standard_product_pipeline_1_full
-# rm -rf -v ^(_context.json|S1*_IW_SLC_*.zip|docker-compose.yaml|*.sh|*.py)
+# rm -rf -v ^(_context.json|S1*_IW_SLC_*.zip|docker-compose.yaml|*.sh|*.py|*.EOF)
 # cd ../coseismic_standard_product_pipeline_2_full
-# rm -rf -v ^(_context.json|S1*_IW_SLC_*.zip|docker-compose.yaml|*.sh|*.py)
+# rm -rf -v ^(_context.json|S1*_IW_SLC_*.zip|docker-compose.yaml|*.sh|*.py|*.EOF)
 
 cd ..
