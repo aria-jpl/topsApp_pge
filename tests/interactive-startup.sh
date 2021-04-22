5f8930e73ea214d7a196449536cd5dd99bac94cb
cmarshak <charles.marshak@gmail.com>
2021-04-22 12:21:34 -0700

    remove netrc in tests and use pip to install jupyter

--- tests/interactive-startup.sh
+++ tests/interactive-startup.sh
@@ -1,16 +1,22 @@
 export TOPSAPP="/home/ops/topsApp_pge"
-# chmod og-rw $TOPSAPP/.netrc
-yes | cp -rf $TOPSAPP/.netrc $HOME/.netrc
 export PYTHONPATH="$TOPSAPP:$PYTHONPATH"
 
-sudo chown -R $USER:$USER /opt/conda
-conda install -c conda-forge jupyter tmux --yes
+# This is very slow
+# sudo chown -R $USER:$USER /opt/conda
+# conda install -c conda-forge jupyter tmux --yes
+pip install jupyter
 
 source /opt/isce2/isce_env.sh
 export PYTHONPATH="$ISCE_HOME/applications:$ISCE_HOME/components:$BASE_PATH:$ARIAMH_HOME:$PYTHONPATH"
 
-# Make sure you run this in topsApp_pge
+# If you have permission issues,
+# you may run this in topsApp_pge:
+#
+# Remote server
 # sudo chown -R $(id -u):$(id -g) $PWD
+# OR
+#
+# local machine
 # Allow all Read and write access
-cd $TOPSAPP
-chmod -R 0777 $PWD
+# cd $TOPSAPP
+# chmod -R 0777 $PWD
