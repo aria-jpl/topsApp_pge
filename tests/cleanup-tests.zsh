a999f3ffd4771eae833d7b5c646000cdbd0286ec
cmarshak <charles.marshak@gmail.com>
2021-05-25 15:07:21 -0700

    download orbit script and cleanup

--- tests/cleanup-tests.zsh
+++ tests/cleanup-tests.zsh
@@ -9,6 +9,8 @@ cd ../coseismic_standard_product_pipeline_1
 rm -rf -v ^(_context.json|S1*_IW_SLC_*.zip|docker-compose.yaml|*.sh|*.py|*.EOF)
 cd ../coseismic_standard_product_pipeline_2
 rm -rf -v ^(_context.json|S1*_IW_SLC_*.zip|docker-compose.yaml|*.sh|*.py|*.EOF)
+cd ../coseismic_standard_product_pipeline_4_full_R2S2
+rm -rf -v ^(_context.json|S1*_IW_SLC_*.zip|docker-compose.yaml|*.sh|*.py|*.EOF)
 
 # cd ../coseismic_standard_product_pipeline_1_full
 # rm -rf -v ^(_context.json|S1*_IW_SLC_*.zip|docker-compose.yaml|*.sh|*.py|*.EOF)
